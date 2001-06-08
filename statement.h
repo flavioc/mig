47ffdd7cb4fc9cf9908957023f9c7388e65bc552
Roland McGrath <roland@gnu.org>
2001-06-08 00:48:59 +0000

    2001-06-07  Roland McGrath  <roland@frob.com>
    	* statement.h: Fix obsolete #else/#endif syntax.
    	* lexxer.l: Likewise.

--- statement.h
+++ statement.h
@@ -1,25 +1,25 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1991,1990 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS 
+ *
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie the
  * rights to redistribute these changes.
  */
@@ -62,4 +62,4 @@ extern statement_t *stAlloc(void);
 /* list of statements, in order they occur in the .defs file */
 extern statement_t *StatementList;
 
-#endif	_STATEMENT_H
+#endif	/* _STATEMENT_H */
