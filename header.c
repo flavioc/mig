7ac5b7901a929f676620f8e0816047478f9a5b25
Roland McGrath <roland@gnu.org>
2001-06-08 00:49:05 +0000

    2001-06-07  Roland McGrath  <roland@frob.com>
    	* header.c (WriteRoutineList): New function.
    	* write.h: Declare it.
    	* migcom.c (RoutineListFileName): New variable.
    	(parseArgs): New option -list to set it.
    	(main): If set, write the named output file with WriteRoutineList.
    	* mig.in: Grok -list and put it in the usage message.

--- header.c
+++ header.c
@@ -1,25 +1,25 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1991,1990 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS 
+ *
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie the
  * rights to redistribute these changes.
  */
@@ -127,6 +127,23 @@ WriteUserHeader(FILE *file, const statement_t *stats)
 		  (int) stat->stKind);
 	}
     WriteEpilog(file, protect);
+}
+
+void
+WriteRoutineList(FILE *file, const statement_t *stats)
+{
+  const statement_t *stat;
+  for (stat = stats; stat != stNULL; stat = stat->stNext)
+    if (stat->stKind == skRoutine)
+      {
+	const routine_t *rt = stat->stRoutine;
+
+	fprintf (file, "%s %u %s %u %u %u\n",
+		 SubsystemName, SubsystemBase,
+		 rt->rtName, rt->rtNumber,
+		 SubsystemBase + rt->rtNumber,
+		 rt->rtOneWay ? 0 : SubsystemBase + rt->rtNumber + 100);
+      }
 }
 
 static void
