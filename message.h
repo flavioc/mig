9177b4ddd86c36eb861cfdbe2342000d97fb2394
Samuel Thibault <samuel.thibault@ens-lyon.org>
2020-06-18 22:03:58 +0200

    message.h: Replace implementation from Utah with implementation from CMU
    This replaces the implementation from the University of Utah, covered by the
    advertising clause, with the implementation from CMU, picked up from the GNU
    Mach source, which is free from the advertising clause.

--- message.h
+++ message.h
@@ -1,37 +1,40 @@
-/* 
- * Copyright (c) 1994 The University of Utah and
- * the Computer Systems Laboratory at the University of Utah (CSL).
- * All rights reserved.
+/*
+ * Copyright (c) 1992-1987 Carnegie Mellon University
+ * All Rights Reserved.
  *
- * Permission to use, copy, modify and distribute this software is hereby
- * granted provided that (1) source code retains these copyright, permission,
- * and disclaimer notices, and (2) redistributions including binaries
- * reproduce the notices in supporting documentation, and (3) all advertising
- * materials mentioning features or use of this software display the following
- * acknowledgement: ``This product includes software developed by the
- * Computer Systems Laboratory at the University of Utah.''
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
  *
- * THE UNIVERSITY OF UTAH AND CSL ALLOW FREE USE OF THIS SOFTWARE IN ITS "AS
- * IS" CONDITION.  THE UNIVERSITY OF UTAH AND CSL DISCLAIM ANY LIABILITY OF
- * ANY KIND FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
  *
- * CSL requests users of this software to return to csl-dist@cs.utah.edu any
- * improvements that they make and grant CSL redistribution rights.
+ * Carnegie Mellon requests users of this software to return to
  *
- *      Author: Bryan Ford, University of Utah CSL
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ *
+ * any improvements or extensions that they make and grant Carnegie Mellon
+ * the rights to redistribute these changes.
  */
-#ifndef _MIG_MESSAGE_H
-#define _MIG_MESSAGE_H
+/*
+ *	Mach IPC message and primitive function definitions.
+ */
 
-/* This file is a substitute for mach/message.h,
-   to be used within MIG sources compiled for the build machine.
-   This way we avoid pulling in all the bogus typedefs and stuff
-   that would normally come with mach/message.h.  */
+#ifndef	_MIG_MESSAGE_H_
+#define _MIG_MESSAGE_H_
 
-#include "cpu.h"
+/*
+ *	Is a given item a port type?
+ */
 
 #define MACH_MSG_TYPE_PORT_ANY(x)			\
 	(((x) >= MACH_MSG_TYPE_MOVE_RECEIVE) &&		\
 	 ((x) <= MACH_MSG_TYPE_MAKE_SEND_ONCE))
 
-#endif /* _MIG_MESSAGE_H */
+#endif	/* _MIG_MESSAGE_H_ */
