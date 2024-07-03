39baf09b58d626d46be3da79a3ab4f41deb37426
Flavio Cruz <flaviocruz@gmail.com>
2024-07-03 21:38:43 +0100

    Make the MiG test suite compatible with GCC 14.
    We need to expose the function declarations in mig_support.h.
    
    Also fix test setup to work well in cases the build directory is not
    inside the source directory.
    Message-ID: <jjbpmhtxy4nybcmhlzueeenyv5taj4zygmsx6w336ggzcbr5yx@tnkp2ycfkf3t>

--- tests/includes/mach/mig_support.h
+++ tests/includes/mach/mig_support.h
@@ -1,1 +1,17 @@
-/* This file allows the testsuite to compile under Linux.  */
+/* This file allows the testsuite to compile under Linux
+ * which doesn't have mach/mig_support.h */
+
+#ifndef	_MACH_MIG_SUPPORT_H
+
+#define	_MACH_MIG_SUPPORT_H 1
+
+#include <mach/std_types.h>
+#include <string.h>
+
+mach_port_t mig_get_reply_port (void);
+void mig_dealloc_reply_port (mach_port_t);
+void mig_put_reply_port (mach_port_t);
+void mig_allocate (vm_address_t *__addr_p, vm_size_t __size);
+void mig_deallocate (vm_address_t __addr, vm_size_t __size);
+
+#endif
